bb8e314874cda19c5b59f859dc082f4a2c17951b
samir <sc9018@ic.ac.uk>
2024-04-21 20:19:26 +0100

    added basic move

--- src/snake/game_objects.py
+++ src/snake/game_objects.py
@@ -17,6 +17,17 @@ class Snake(pygame.sprite.Sprite):
         """
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image("square.png ")
+        self.move_pos = [0,0]
+
+    def update(self):
+        newpos = self.rect.move(self.move_pos)
+        self.rect = newpos
+        pygame.event.pump()
+
+    def move_snake(self,direction=None):
+        # self.move_pos[0] += 32
+        self.move_pos[0] += 32
+
 
 if __name__ == "__main__":
     pass
