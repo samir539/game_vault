6afac909ed0c3471213b85e5c151ea5bd613e4f9
samir <sc9018@ic.ac.uk>
2024-04-25 09:02:36 +0100

    cloud_movement

--- src/x_platformer/game.py
+++ src/x_platformer/game.py
@@ -10,6 +10,7 @@ from einops import rearrange
 from socket import *
 from pygame.locals import *
 import numpy as np
+from resource_handling.image_load import load_image 
 
 
 SCREENWIDTH = 840
@@ -31,19 +32,40 @@ class Game():
         self.clock = pygame.time.Clock()
         self.screen.blit(self.background,(0,0))
         pygame.display.flip()
+        
+        #load cloud
+        self.cloud,_ = load_image("images/clouds/cloud_1.png")
+        self.cloud_pos = [50,50]
+        self.cloud_move = [False,False]
                 
     
     def run_game(self):
         """method to run the game"""
         while True:
             self.clock.tick(FPS)
+            print("this is cloud move bool",self.cloud_move)
+            self.cloud_pos[1] += (self.cloud_move[1] - self.cloud_move[0])*5
+            self.screen.blit(self.cloud,self.cloud_pos) 
             
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     return
-                else:
-                    pass
+                if event.type == pygame.KEYDOWN:
+                    if event.key in [pygame.K_a,pygame.K_w,pygame.K_s,pygame.K_d]:
+                        if event.key == pygame.K_w:
+                            self.cloud_move[0] = True
+                        if event.key == pygame.K_s:
+                            self.cloud_move[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        print("THIS HAPPENED")
+                        self.cloud_move[0] = False
+                    if event.key == pygame.K_s:
+                        self.cloud_move[1] = False
                 
+        
+            
+               
             pygame.display.flip()
 
 
