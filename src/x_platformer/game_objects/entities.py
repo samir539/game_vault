5b24fb5a18526ed3e028532823610bc92195b534
samir <sc9018@ic.ac.uk>
2024-04-25 10:04:15 +0100

    added basic physical entity class

---
+++ src/x_platformer/game_objects/entities.py
@@ -0,0 +1,31 @@
+import sys
+import random
+import math
+import os
+import getopt
+import pygame
+from einops import rearrange
+from socket import *
+from pygame.locals import *
+import numpy as np
+
+
+
+class physicalEntity():
+    def __init__(self,game,pos):
+        self.game = game
+        self.pos = list(pos)
+        self.velocity = [1,0]        
+    
+    def update(self,movement=[0,0]):
+        self.moved_pos = [movement[0] + self.velocity[0], movement[1] + self.velocity[1]]
+        print("this is moved pos", self.moved_pos)
+        self.pos[0] += self.moved_pos[0]
+        self.pos[1] += self.moved_pos[1]
+        
+        
+    
+    def render(self,surface):
+        surface.blit(self.game.assets["player"],self.pos)
+    
+    
