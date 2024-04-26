0e43353192120e508c7cc2ebcb839479e4b44b3b
samir <sc9018@ic.ac.uk>
2024-04-26 14:17:38 +0100

    added loadmultiple images

--- src/x_platformer/resource_handling/image_load.py
+++ src/x_platformer/resource_handling/image_load.py
@@ -7,29 +7,48 @@ import pygame
 
 
 
-def load_image(image):
+def load_image(image_path):
     """
     function to load in a png image
     :param: image name
     :return image: the pygame image object
     :return image.get_rect(): image rectangle coordinates
     """
-    full_name = os.path.join("data",image)
+    # full_name = os.path.join(image_path)
     try:
-        image = pygame.image.load(full_name)
+        image = pygame.image.load(image_path)
         if image.get_alpha() is None:
             image.convert()
         else:
             image.convert_alpha()
     except FileNotFoundError:
-        print(f"the desired file was not found, filename: {full_name}")
+        print(f"the desired file was not found, at the filepath: {image_path}")
         raise SystemExit
     return image, image.get_rect()
 
 
+def load_multiple_images(path):
+    """
+    Function to load all images in a directory into a list
+    """
+    image_list =  os.listdir(path)
+    pg_image_list = []
+    for i in image_list:
+        pg_img = pygame.image.load(path + "/" +i)
+        if pg_img.get_alpha() is None:
+            pg_img.convert()
+        else:
+            pg_img.convert_alpha()
+        pg_image_list.append(pg_img)
+    return pg_image_list
+
+    
+    # image_list = pygame.image.load()
+    # pass
     
 
 
 
 if __name__ == "__main__":
-    pass
+    # load_multiple_images("../data/images/clouds")
+    load_image("../data/images/entities/player/idle/00.png")
