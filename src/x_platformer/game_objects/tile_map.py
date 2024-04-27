ecb41b3cdb4d17446f7ce7f4388fad7e058bbac0
samir <sc9018@ic.ac.uk>
2024-04-27 12:21:33 +0100

    added four neighbour recogition

--- src/x_platformer/game_objects/tile_map.py
+++ src/x_platformer/game_objects/tile_map.py
@@ -8,17 +8,23 @@ from einops import rearrange
 from socket import *
 from pygame.locals import *
 import numpy as np
-# from .. resource_handling.image_load import load_image
+from game_functions.collision_checks import check_side
 
 class TileMap():
+    """
+    tile are assumed to be squares
+    """
     def __init__(self,game,tile_size):
         self.game = game
         self.tile_size = tile_size
         self.tilemap = {}
         self.offtile = []
+        self.surr_dir = [[0,-1],[1,0],[0,1],[-1,0]] #directions of four nearest tiles
         
-        for i in range(1,10):
-            self.tilemap[f"{i},10"] = {"tile_type":"grass_tiles", "tile_edition":2,"pos":[i,15]}
+        for i in range(2,23):
+            self.tilemap[f"{i},13"] = {"tile_type":"grass_tiles", "tile_edition":2,"pos":[i,13]}
+        for i in range(1,25):
+            self.tilemap[f"10,{i}"] = {"tile_type":"stone_tiles", "tile_edition":2,"pos":[10,i]}
     
     def render_tiles(self,surface):
         for i in self.tilemap.values():
@@ -26,6 +32,14 @@ class TileMap():
             pos_x = i["pos"][0]*self.tile_size
             pos_y = i["pos"][1]*self.tile_size
             surface.blit(tile, (pos_x,pos_y))
+            
+            
+    def tile_collision_check(self):
+        player_pos = self.game.player_1.pos
+        player_tile =[int(x/self.tile_size) for x in player_pos]
+        surrounding_tiles = [[player_tile[0] + x[0] ,player_tile[1] + x[1]] for x in self.surr_dir]
+        
+        
         
     
 if __name__ == "__main__":
