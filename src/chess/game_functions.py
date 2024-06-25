f081dd56f8301ace13d6aca9de1e3d7e97505275
samir <sc9018@ic.ac.uk>
2024-06-25 13:43:06 +0100

    changed pieces from list to dict
    each white_pieces and black_pieces container now has the location as the key and the object piece as the value

--- src/chess/game_functions.py
+++ src/chess/game_functions.py
@@ -14,35 +14,41 @@ def make_pieces():
     bishop_starting_files = ["c","f"]
     knight_starting_files = ["b","g"]
     
-    white_pieces = []
-    black_pieces = []
+    white_pieces = {}
+    black_pieces = {}
     
     #add pawns
     for i in board_width:
-        white_pieces.append(Pawn("white",f"{i}{pawn_row_list[0]}",True))
-        black_pieces.append(Pawn("black",f"{i}{pawn_row_list[1]}",True))
+        white_pieces[f"{i}{pawn_row_list[0]}"] = Pawn("white",f"{i}{pawn_row_list[0]}",True)
+        black_pieces[f"{i}{pawn_row_list[1]}"] = Pawn("black",f"{i}{pawn_row_list[1]}",True)
         
     #add rooks
     for i in rook_starting_files:
-        white_pieces.append(Rook("white",f"{i}{starting_row_list[0]}",True))
-        black_pieces.append(Rook("white",f"{i}{starting_row_list[1]}",True))
+        white_pieces[f"{i}{starting_row_list[0]}"] = Rook("white",f"{i}{starting_row_list[0]}",True)
+        black_pieces[f"{i}{starting_row_list[1]}"] = Rook("white",f"{i}{starting_row_list[1]}",True)
         
     #add knights
     for i in knight_starting_files:
-        white_pieces.append(Knight("white",f"{i}{starting_row_list[0]}",True))
-        black_pieces.append(Knight("white",f"{i}{starting_row_list[1]}",True))
+        white_pieces[f"{i}{starting_row_list[0]}"] = Knight("white",f"{i}{starting_row_list[0]}",True)
+        black_pieces[f"{i}{starting_row_list[1]}"] = Knight("white",f"{i}{starting_row_list[1]}",True)
       
     #add bishops
     for i in bishop_starting_files:
-        white_pieces.append(Bishop("white",f"{i}{starting_row_list[0]}",True))
-        black_pieces.append(Bishop("white",f"{i}{starting_row_list[1]}",True))  
+        white_pieces[f"{i}{starting_row_list[0]}"] = Bishop("white",f"{i}{starting_row_list[0]}",True)
+        black_pieces[f"{i}{starting_row_list[1]}"] = Bishop("white",f"{i}{starting_row_list[1]}",True)  
         
     #add queens
-    white_pieces.append(Queen("white",f"d{starting_row_list[0]}",True))
-    black_pieces.append(Queen("black",f"d{starting_row_list[1]}",True))
+    white_pieces[f"d{starting_row_list[0]}"]  = Queen("white",f"d{starting_row_list[0]}",True)
+    black_pieces[f"d{starting_row_list[1]}"] = Queen("black",f"d{starting_row_list[1]}",True)
     
     #add kings
-    white_pieces.append(King("white",f"e{starting_row_list[0]}",True))
-    black_pieces.append(King("black",f"e{starting_row_list[1]}",True))
+    white_pieces[f"e{starting_row_list[0]}"] = King("white",f"e{starting_row_list[0]}",True)
+    black_pieces[f"e{starting_row_list[1]}"] = King("black",f"e{starting_row_list[1]}",True)
     
     return white_pieces,black_pieces
+
+if __name__ == "__main__":
+    a,b = make_pieces()
+    print(a)
+    
+    print(a["f1"])
