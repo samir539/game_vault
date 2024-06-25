517d5a1bf1d3843a166f922dc1364ced0f03400c
samir <sc9018@ic.ac.uk>
2024-06-25 15:37:48 +0100

    added input check on proposed move

--- src/chess/game_functions.py
+++ src/chess/game_functions.py
@@ -1,6 +1,6 @@
 
 from pieces import AbstractChessPiece,Pawn,Rook,Knight,Bishop,Queen,King
-from board import Board
+
 
 def make_pieces():
     """
@@ -25,17 +25,17 @@ def make_pieces():
     #add rooks
     for i in rook_starting_files:
         white_pieces[f"{i}{starting_row_list[0]}"] = Rook("white",f"{i}{starting_row_list[0]}",True)
-        black_pieces[f"{i}{starting_row_list[1]}"] = Rook("white",f"{i}{starting_row_list[1]}",True)
+        black_pieces[f"{i}{starting_row_list[1]}"] = Rook("black",f"{i}{starting_row_list[1]}",True)
         
     #add knights
     for i in knight_starting_files:
         white_pieces[f"{i}{starting_row_list[0]}"] = Knight("white",f"{i}{starting_row_list[0]}",True)
-        black_pieces[f"{i}{starting_row_list[1]}"] = Knight("white",f"{i}{starting_row_list[1]}",True)
+        black_pieces[f"{i}{starting_row_list[1]}"] = Knight("black",f"{i}{starting_row_list[1]}",True)
       
     #add bishops
     for i in bishop_starting_files:
         white_pieces[f"{i}{starting_row_list[0]}"] = Bishop("white",f"{i}{starting_row_list[0]}",True)
-        black_pieces[f"{i}{starting_row_list[1]}"] = Bishop("white",f"{i}{starting_row_list[1]}",True)  
+        black_pieces[f"{i}{starting_row_list[1]}"] = Bishop("black",f"{i}{starting_row_list[1]}",True)  
         
     #add queens
     white_pieces[f"d{starting_row_list[0]}"]  = Queen("white",f"d{starting_row_list[0]}",True)
@@ -52,3 +52,26 @@ if __name__ == "__main__":
     print(a)
     
     print(a["f1"])
+    
+    
+def format_of_move_valid(move):
+    """
+    check if proposed move could be valid (still does not mean it is necessarily valid)
+    """
+    if len(move) != 4:
+        return False
+    
+    start_file = move[0]
+    start_rank = move[1]
+    end_file = move[2]
+    end_rank = move[3]
+    
+    # check between 'a' and 'h'
+    if start_file not in 'abcdefgh' or end_file not in 'abcdefgh':
+        return False
+    
+    # check '1' and '8'
+    if start_rank not in '12345678' or end_rank not in '12345678':
+        return False
+
+    return True
