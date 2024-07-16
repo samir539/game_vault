df7580fb7cb4db8e498c4fe14663d3cc41e9e4f3
samir <sc9018@ic.ac.uk>
2024-07-16 10:46:16 +0100

    cleaned update_board

--- src/chess/board.py
+++ src/chess/board.py
@@ -8,12 +8,13 @@ sys.stdout = codecs.getwriter("utf-8")(sys.stdout.buffer, 'strict')
 nums_to_letters = {1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h"}
 
 #white 1
-#black
+#black 0
 
 class Board:
     def __init__(self,pieces_white:list,pieces_black:list):
         self._pieces_white = pieces_white
         self._pieces_black = pieces_black
+        self._target_pieces = lambda player_turn: self._pieces_black if player_turn else self._pieces_white
         
     
     @property
@@ -32,17 +33,50 @@ class Board:
         2.) check requested piece is the correct colour
         3.) check if moving requested piece opens up a check 
         """    
+        #check if input has correct format
         query_valid = True if format_of_move_valid(given_move) else False
+
+        #check if requested piece is on the board and of the correct colour
         if player_turn == 1:
             piece_on_board = True if given_move[-2:] in self._pieces_white else False
         else:
             piece_on_board = True if given_move[-2:] in self._pieces_black else False
+        
+        #look if move reveals check
         not_reveal_check = True #Come back to implementing this 
         
+        #check if move end location is on a same coloured piece
+        
+
+        #valid check
         if not_reveal_check and piece_on_board and query_valid:
             return True
         
         return False
+    
+    def _look_for_check(self):
+        pass
+    
+    def _look_for_checkmate(self,player_turn):
+        """
+        give a certain players turn, check to see if the move carried out renders the oppoenent into a state of checkmate
+        """
+        #check all 9 surrounding squares to see if any still yield a check 
+        #get pos of king
+        #get surr 9 squares
+        def get_king_pos():
+            for i in self._target_pieces(player_turn).values():
+                print(i)
+            for pos,piece in self._target_pieces(player_turn).items():
+                if piece.is_king():
+                    return pos
+                
+        king_pos = get_king_pos()
+        print(king_pos,type(king_pos),list(king_pos))
+        surr_grid = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]
+        
+                
+        
          
         
         
@@ -54,26 +88,41 @@ class Board:
         3.) carry out the move and update board state
         gives a false if the move is not possible otherwise moves the piece and makes any associated neccesasry changes
         """
+        #check if valid move
         if not self._check_valid_piece(given_move,player_turn):
             return False
 
+        #get the piece in question
         piece = self._pieces_white[given_move[:2]] if player_turn  else self._pieces_black[given_move[:2]]
         
+        #check if move is valid from the piece POV
         if not piece.move(self.pieces_white,self.pieces_black,start_position=given_move[:2],end_position=given_move[-2:]):
             return False
         else:
-            #update board
-            pass
+            
+            #check if move is a capture move 
+            capture = False
+            if player_turn:
+                if given_move[-2:] in self._pieces_black:
+                    capture = True
+            else:
+                if given_move[-2:] in self._pieces_white:
+                    capture = True
+            
+            #delete captured piece from board
+            if capture == True:
+                del self._target_pieces(player_turn)[given_move[-2:]]
 
-        
-        
-        #if valid ->  make move and return True
-        #if not valid -> return False
-        #move piece, if piece captures another remove captured piece
-        #identify the piece in question
-        #check it has the colour required
-        #check if it can be moved (check considerations)
-        pass
+            #move piece to end location
+            self._target_pieces(player_turn)[given_move[-2:]] = self._target_pieces(player_turn).pop(given_move[:2])
+            
+            #look to see if new move renders checkmate
+
+            #look to see if new move renders check
+            
+            
+
+            
     
     
     def render(self):
@@ -113,6 +162,9 @@ class Board:
             
 if __name__ == "__main__":
     white_pieces,black_pieces = make_pieces()
+    
     myBoard = Board(white_pieces,black_pieces)
-    myBoard.render()
+    print(myBoard._target_pieces(1))
+    myBoard._look_for_checkmate(1)
+    # myBoard.render()
     
