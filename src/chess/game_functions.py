8c11c827d5af2484b755e89d8f561167a88c913b
samir <sc9018@ic.ac.uk>
2024-07-18 11:39:00 +0100

    modified make pieces to work with dynamicdict

--- src/chess/game_functions.py
+++ src/chess/game_functions.py
@@ -2,7 +2,7 @@
 from pieces import AbstractChessPiece,Pawn,Rook,Knight,Bishop,Queen,King
 import sys
 import codecs
-
+from support_functions import DynamicKeyDict
 # Setting the output encoding to UTF-8
 # sys.stdout = codecs.getwriter("utf-8")(sys.stdout.buffer, 'strict')
 
@@ -27,36 +27,38 @@ def make_pieces():
     queen_starting_file = 4
     king_starting_file = 5
     
-    white_pieces = {}
-    black_pieces = {}
+    white_pieces = DynamicKeyDict()
+    black_pieces = DynamicKeyDict()
     
     #add pawns
     for i in board_width:
-        white_pieces[concat(pawn_row_list[0],i)] = Pawn("white",concat(pawn_row_list[0],i),True)
-        black_pieces[concat(pawn_row_list[1],i)] = Pawn("black",concat(pawn_row_list[1],i),True)
+        white_pieces.add(Pawn("white",concat(pawn_row_list[0],i),True)) 
+        black_pieces.add(Pawn("black",concat(pawn_row_list[1],i),True)) 
+        
+
         
     #add rooks
     for i in rook_starting_files:
-        white_pieces[concat(starting_row_list[0],i)] = Rook("white",concat(starting_row_list[0],i),True)
-        black_pieces[concat(starting_row_list[1],i)] = Rook("black",concat(starting_row_list[1],i),True)
+        white_pieces.add(Rook("white",concat(starting_row_list[0],i),True))  
+        black_pieces.add(Rook("black",concat(starting_row_list[1],i),True)) 
         
     #add knights
     for i in knight_starting_files:
-        white_pieces[concat(starting_row_list[0],i)] = Knight("white",concat(starting_row_list[0],i),True)
-        black_pieces[concat(starting_row_list[1],i)] = Knight("black",concat(starting_row_list[1],i),True)
+        white_pieces.add(Knight("white",concat(starting_row_list[0],i),True))  
+        black_pieces.add(Knight("black",concat(starting_row_list[1],i),True)) 
       
     #add bishops
     for i in bishop_starting_files:
-        white_pieces[concat(starting_row_list[0],i)] = Bishop("white",concat(starting_row_list[0],i),True)
-        black_pieces[concat(starting_row_list[1],i)] = Bishop("black",concat(starting_row_list[1],i),True)  
+        white_pieces.add(Bishop("white",concat(starting_row_list[0],i),True)) 
+        black_pieces.add(Bishop("black",concat(starting_row_list[1],i),True))
         
     #add queens
-    white_pieces[concat(starting_row_list[0],queen_starting_file)]  = Queen("white",concat(starting_row_list[0],queen_starting_file),True)
-    black_pieces[concat(starting_row_list[1],queen_starting_file)] = Queen("black",concat(starting_row_list[1],queen_starting_file),True)
+    white_pieces.add(Queen("white",concat(starting_row_list[0],queen_starting_file),True)) 
+    black_pieces.add(Queen("black",concat(starting_row_list[1],queen_starting_file),True))  
     
     #add kings
-    white_pieces[concat(starting_row_list[0],king_starting_file)] = King("white",concat(starting_row_list[0],king_starting_file),True)
-    black_pieces[concat(starting_row_list[1],king_starting_file)] = King("black",concat(starting_row_list[1],king_starting_file),True)
+    white_pieces.add(King("white",concat(starting_row_list[0],king_starting_file),True)) 
+    black_pieces.add(King("black",concat(starting_row_list[1],king_starting_file),True)) 
     
     return white_pieces,black_pieces
 
@@ -68,5 +70,5 @@ def make_pieces():
 
 if __name__ == "__main__":
     a,b = make_pieces()
-    # print(a)
-    print(type(b))
+    print(a)
+    print(b)
